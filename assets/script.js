1638521439d8161ec09c21e44566a0b6304bd056
joshua.p.ward2 <joshua.p.ward2@gmail.com>
2022-07-06 12:44:01 -0400

    questions added to script

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,34 @@
-document.getElementById("oneofone").addEventListener("click", changecolor)
-function changecolor() {
-    document.getElementById("oneofone").innerText = "CLICK";
-    document.getElementById("oneofone").style. backgroundColor = "orange";
-}
+var myQuestions = [
 
+    {
 
+        Question:  "What three vegetables make up a mirepoix? ",
+        answers:  ["Carrots Potatoes Greeen Beans", "Potatoes Cabbage Celery", "Carrots Onions Celery", "Celery Green Beans Carrots" ],
+        correct: 3  
 
-.innerHtml = ""
+    },
+    {
+        Question: "What is the only food that can never go bad?",
+         answers: ["Onions", "Honey", "Coffee beans", "Bread"],
+        coreect: 2
+    },
+    {
+        Question: "Whats the only fruit with seeds on the outside?",
+        answers: ["Pineapple", "Kiwi", "Apple", "Strawberry"],
+        correct: 4
+    },
+    {
+        Question: "Where were french fries invented?",
+        answers: ["France", "Spain", "Italy", "Belgium" ],
+        correct: 4
+
+        
+    }
+    {
+        Question: "What is the only US state where coffee beans are grown?",
+        answers: ["Texas", "New Mexico", "Hawaii", "Oregon"],
+        correct: 3
+    }
+
+];
 
